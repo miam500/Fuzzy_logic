a0fe8a2b2d295e9b7ec5dac1fc622ff161d9922b
miam500 <78931755+miam500@users.noreply.github.com>
2022-11-09 13:56:23 -0500

    Update fuzzyLogic.py

--- fuzzyLogic.py
+++ fuzzyLogic.py
@@ -4,8 +4,12 @@ import numpy as np
 
 def createFuzzyController():
     # TODO: add in
-
+    mic_0 = ctrl.Antecedent(np.linspace(0, 10), 'mic_0')
+    mic_1 = ctrl.Antecedent(np.linspace(0, 10), 'mic_1')
+    mic_2 = ctrl.Antecedent(np.linspace(0, 10), 'mic_2')
     # TODO: add out
+    x = ctrl.Consequent(np.linspace(0, 1), 'x')
+    y = ctrl.Consequent(np.linspace(0, 1), 'y')
     
     # TODO: membership function
     
