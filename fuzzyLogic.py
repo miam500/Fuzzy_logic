8da834ccc2c0d7b398a74101aa7859d463abc691
miam500 <78931755+miam500@users.noreply.github.com>
2022-11-09 13:43:26 -0500

    Update fuzzyLogic.py

--- fuzzyLogic.py
+++ fuzzyLogic.py
@@ -1,1 +1,22 @@
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+import numpy as np
 
+def createFuzzyController():
+    # TODO: add in
+
+    # TODO: add out
+    
+    # TODO: membership function
+    
+    # TODO: define rules
+    rules = []
+    rules.append(ctrl.Rule()) # ex
+    
+    for rule in rules:
+        rule.and_func = np.fmin
+        rule.or_func = np.fmax
+    
+    system = ctrl.ControlSystem(rules=rules)
+    sim = ctrl.ControlSystemSimulation(system)
+    return sim
