658a6d18a1f31262dd32238b1d939d844bde84a1
miam500 <78931755+miam500@users.noreply.github.com>
2022-12-14 16:07:05 -0500

    Update fuzzyLogic_y.py

--- fuzzyLogic_y.py
+++ fuzzyLogic_y.py
@@ -1,44 +1,98 @@
+import pygame
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import numpy as np
+from map import *
 
-def createFuzzyController():
-    # TODO: add in
-    mic0 = ctrl.Antecedent(np.linspace(0, 10), 'mic0')
-    mic1 = ctrl.Antecedent(np.linspace(0, 10), 'mic1')
-    mic2 = ctrl.Antecedent(np.linspace(0, 10), 'mic2')
-    # TODO: add out
-    x = ctrl.Consequent(np.linspace(-1, 1), 'x')
-    y = ctrl.Consequent(np.linspace(-1, 1), 'y')
-    # TODO: membership function
-    names = ['VF', 'F', 'M', 'C', 'VC']
-    mic0.automf(names=names)
-    mic1.automf(names=names)
-    mic2.automf(names=names)
-    
-    names = ['L', 'M', 'R']
-    x.automf(names=names)
-    names = ['U', 'M', 'D']
-    y.automf(names=names)
-    
-    # TODO: define rules
-    rules = []
-    ##############################################
-    #                   mic0                     #
-    ##############################################
-    rules.append(ctrl.Rule(antecedent=(mic0['VF'] & mic1[''] & mic2[''])))
-    ##############################################
-    #                   mic1                     #
-    ##############################################
-    ##############################################
-    #                   mic2                     #
-    ##############################################
+class createFuzzyController():
+    def __init__(self, grid_cells, mic, sampling_grid=2, membership_fcns=5):
+        self.n_membership = membership_fcns
+        self.grid = grid_cells
+        self.mic = mic
+        self.size = 30
+        self.color = pygame.Color('red')
+        self.sampling_grid = sampling_grid
 
-    for rule in rules:
-        rule.and_func = np.fmin
-        rule.or_func = np.fmax
+        # TODO: add in
+        mic = ctrl.Antecedent(np.linspace(0, 60), 'mic')
+        x = ctrl.Antecedent(np.linspace(0, WIDTH), 'x')
+        y = ctrl.Antecedent(np.linspace(0, HEIGHT), 'y')
+        # TODO: add out
+        x_out = ctrl.Consequent(np.linspace(0, WIDTH), 'x_out')
+        y_out = ctrl.Consequent(np.linspace(0, HEIGHT), 'y_out')
+        # TODO: membership function
+        names = ['F', 'M', 'C']
+        mic.automf(names=names)
+        names = ['L', 'R']
+        x.automf(names=names)
+        names = ['U', 'D']
+        y.automf(names=names)
 
-    system = ctrl.ControlSystem(rules=rules)
-    sim = ctrl.ControlSystemSimulation(system)
-    return sim
+        #names = ['LL', 'L', 'R', 'RR']
+        #x_out.automf(names=names)
+        x_out['L'] = fuzz.trimf(x_out.universe, [0, 0, self.mic.x])
+        x_out['M'] = fuzz.trimf(x_out.universe, [self.mic.x/2, self.mic.x, self.mic.x+(WIDTH - self.mic.x)/2])
+        x_out['R'] = fuzz.trimf(x_out.universe, [self.mic.x, WIDTH, WIDTH])
+        #names = ['UU', 'U', 'D', 'DD']
+        #y_out.automf(names=names)
+        y_out['U'] = fuzz.trimf(y_out.universe, [0, 0, self.mic.y])
+        y_out['M'] = fuzz.trimf(y_out.universe, [self.mic.y/2, self.mic.y, self.mic.y + (HEIGHT - self.mic.y)/2])
+        y_out['D'] = fuzz.trimf(y_out.universe, [self.mic.y, HEIGHT, HEIGHT])
 
+        # TODO: define rules
+        rules = []
+        ##############################################
+        #                   mic F                    #
+        ##############################################
+        rules.append(ctrl.Rule(antecedent=(mic['F'] & x['L']), consequent=(x_out['R'])))
+        rules.append(ctrl.Rule(antecedent=(mic['F'] & y['U']), consequent=(y_out['D'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['F'] & x['L']), consequent=(x_out['R'])))
+        rules.append(ctrl.Rule(antecedent=(mic['F'] & y['D']), consequent=(y_out['U'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['F'] & x['R']), consequent=(x_out['L'])))
+        rules.append(ctrl.Rule(antecedent=(mic['F'] & y['U']), consequent=(y_out['D'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['F'] & x['R']), consequent=(x_out['L'])))
+        rules.append(ctrl.Rule(antecedent=(mic['F'] & y['D']), consequent=(y_out['U'])))
+
+        ##############################################
+        #                   mic M                    #
+        ##############################################
+        rules.append(ctrl.Rule(antecedent=(mic['M'] & x['L']), consequent=(x_out['R'])))
+        rules.append(ctrl.Rule(antecedent=(mic['M'] & y['U']), consequent=(y_out['D'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['M'] & x['L']), consequent=(x_out['R'])))
+        rules.append(ctrl.Rule(antecedent=(mic['M'] & y['D']), consequent=(y_out['U'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['M'] & x['R']), consequent=(x_out['L'])))
+        rules.append(ctrl.Rule(antecedent=(mic['M'] & y['U']), consequent=(y_out['D'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['M'] & x['R']), consequent=(x_out['L'])))
+        rules.append(ctrl.Rule(antecedent=(mic['M'] & y['D']), consequent=(y_out['U'])))
+        ##############################################
+        #                   mic C                    #
+        ##############################################
+        rules.append(ctrl.Rule(antecedent=(mic['C'] & x['L']), consequent=(x_out['M'])))
+        rules.append(ctrl.Rule(antecedent=(mic['C'] & y['U']), consequent=(y_out['M'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['C'] & x['L']), consequent=(x_out['M'])))
+        rules.append(ctrl.Rule(antecedent=(mic['C'] & y['D']), consequent=(y_out['M'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['C'] & x['R']), consequent=(x_out['M'])))
+        rules.append(ctrl.Rule(antecedent=(mic['C'] & y['U']), consequent=(y_out['M'])))
+
+        rules.append(ctrl.Rule(antecedent=(mic['C'] & x['R']), consequent=(x_out['M'])))
+        rules.append(ctrl.Rule(antecedent=(mic['C'] & y['D']), consequent=(y_out['M'])))
+
+        for rule in rules:
+            rule.and_func = np.fmin
+            rule.or_func = np.fmax
+
+        system = ctrl.ControlSystem(rules=rules)
+        sim = ctrl.ControlSystemSimulation(system)
+
+        self.sim = sim
+
+    def get_input(self):
+        return self.mic.data
