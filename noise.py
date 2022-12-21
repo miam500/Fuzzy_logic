bf5be9be21b4878100763c83c81318ee643756be
Tristan-01 <59707608+Tristan-01@users.noreply.github.com>
2022-12-21 14:45:08 -0500

    Update noise.py

--- noise.py
+++ noise.py
@@ -15,6 +15,39 @@ class Noise:
         amp = self.get_amplitude(t)
         dB = amp - 20*math.log10(distance/1)
         return  dB
+    
+    def propagate_sound_old(self,pos,grid_cells,mics,t,dist = 0 ,come_from = None):
+
+        from map import TILE
+        x,y = pos
+        ix = int((x) // TILE)
+        iy = int((y) // TILE)
+
+        cell = grid_cells[ix][iy]
+        if cell.dist > dist:
+
+            cell.dist = dist
+            cell.pos = pos
+
+            dist_to_left =  x - (ix * TILE)
+            dist_to_right =  ((ix+1) * TILE) - x
+            dist_to_top =  y - (iy * TILE)
+            dist_to_bot =  ((iy+1) * TILE) - y
+
+            order = np.argsort([dist_to_left, dist_to_right, dist_to_top, dist_to_bot])
+            for i in order:
+                if not cell.walls['left'] and i == 0 and not come_from == 'right':
+                    pos = (x - dist_to_left - 1, y)
+                    self.propagate_sound(pos, grid_cells, mics, t, dist + dist_to_left, 'left')
+                if not cell.walls['right'] and i == 1 and not come_from == 'left':
+                    pos = (x + dist_to_right + 1, y)
+                    self.propagate_sound(pos, grid_cells, mics, t, dist + dist_to_right, 'right')
+                if not cell.walls['top'] and i == 2 and not come_from == 'bot':
+                    pos = (x, y - dist_to_top - 1)
+                    self.propagate_sound(pos, grid_cells, mics, t, dist + dist_to_top, 'top')
+                if not cell.walls['bot'] and i == 3 and not come_from == 'top':
+                    pos = (x, y + dist_to_bot + 1)
+                    self.propagate_sound(pos, grid_cells, mics, t, dist + dist_to_bot, 'bot')
 
     def propagate_sound(self,pos,grid_cells,mics,dist = 0,come_from =  None, previous_cells = []):
         
